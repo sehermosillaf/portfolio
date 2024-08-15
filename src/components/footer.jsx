31a85bb0a474eef664b726a957ce00a2511a378f
Sebastian Hermosilla Ferreira <se.hermosillaf@gmail.com>
2024-08-15 18:39:13 -0400

    Crea componente para footer
    @todo: considerar separar mas los components

---
+++ src/components/footer.jsx
@@ -0,0 +1,28 @@
+
+
+export const Footer = () => {
+
+  return (
+    <footer className="opacity-80 m-4 mt-16 w-full mx-auto container lg:max-w-4xl md:max-w-2xl mb-10 flex justify-center">
+      <div className="flex">
+        <ul className="flex">
+          <li className="mx-4">
+            <a href="" className="font-semibold text-sm	 uppercase tracking-wider hover:text-sky-600 transition ease-in-out delay-150 duration-75">
+                Github
+            </a>
+            </li>
+            <li className="mx-4">
+            <a href="" className="font-semibold text-sm	 uppercase tracking-wider hover:text-sky-600 transition ease-in-out delay-150 duration-75">
+                LinkedIn
+            </a>
+            </li>
+            <li className="mx-4">
+            <a href="" className="font-semibold text-sm	 uppercase tracking-wider hover:text-sky-600 transition ease-in-out delay-150 duration-75">
+                Twitter
+            </a>
+            </li>
+        </ul>
+        </div>
+    </footer>
+  )
+}
